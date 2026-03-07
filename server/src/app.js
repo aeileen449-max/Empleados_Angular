1c68040ac6fa50300df8f1135a1081a3ac7934f1
Alden <dilan.pozo.11@gmail.comj>
2026-03-07 12:06:56 -0500

    Semana 3

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 app.use(morgan('dev'));
-app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.set('port', process.env.PORT || 4000);
 app.use('/api/employees',
     require('./routes/employess.routes'));
 module.exports = app;
